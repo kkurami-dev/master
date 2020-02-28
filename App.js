9e8085456e62fda96f4f6fc30bb995b125cce3f5
kazu <a@b.com>
2020-02-29 01:23:33 +0900

    ファイルの読み込み場所の追加準備

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+//import screens
+import Single1 from './screens/Single1';
+import Single2 from './screens/Single2';
+import Stack1 from './screens/Stack1';
+import Stack2 from './screens/Stack2';
+import Tab1 from './screens/Tab1';
+import Tab2 from './screens/Tab2';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Single1/>
+        );
+    }
+}
