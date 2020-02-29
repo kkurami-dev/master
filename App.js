3e2ac4b226d168d23b21388a5f16915916f61aa2
kazu <a@b.com>
2020-03-01 06:47:32 +0900

    タブが表示されつづける

--- App.js
+++ App.js
@@ -3,54 +3,81 @@ import { createAppContainer } from 'react-navigation';
 import { createDrawerNavigator, DrawerActions } from 'react-navigation-drawer';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Page1Screen from './screens/Page1Screen';
 import Page1DetailScreen from './screens/Page1DetailScreen';
 import Page2Screen from './screens/Page2Screen';
 import Single1 from './screens/Single1';
+import Single2 from './screens/Single2';
 
 // setting main nav
 const MainStack = createStackNavigator(
-  {
-    Page1: Page1Screen,
-    Page1Detail: Page1DetailScreen,
-  },
-  {
-    initialRouteName: 'Page1'
-  }
+    {
+        Page1: {
+            screen: Page1Screen,
+            navigationOptions: ({ navigation }) => ({
+                headerLeft: (
+                        <Icon name="bars"
+                    size={24}
+                    onPress={() => navigation.toggleLeftDrawer()}
+                    style={{ paddingLeft: 20 }}
+                        />
+                ),
+            })
+        },
+        Page1Detail: Page1DetailScreen,
+    },
+    {
+        initialRouteName: 'Page1'
+    }
 )
 
 // 左Drawer
 const LeftDrawer1 = createDrawerNavigator(
     {
-        LEFT: MainStack,
+        Home: MainStack,
+        Drafts1: Single1,
+        Drafts2: Single2,
     },
     {
-        contentComponent: Single1,
-        drawerPosition: 'left',
+        initialRouteName: 'Home',
+        contentOptions: {
+            activeTintColor: '#e91e63',
+        },
+//        contentComponent: Single1,
+//        drawerPosition: 'left',
+        getCustomActionCreators: (route, stateKey) => {
+            // console.log("LEFT" + stateKey);
+            return {
+                toggleLeftDrawer: () => DrawerActions.toggleDrawer({ key: stateKey }),
+            };
+        },
     }
 );
 
 // tab nav setting
-const TabNavigator = createBottomTabNavigator(
-  {
-    Page1: {
-      screen: LeftDrawer1
+//const TabNavigator = createBottomTabNavigator(
+const TabNavigator = createMaterialBottomTabNavigator(
+    {
+        Page1: {
+            screen: LeftDrawer1
+        },
+        Page2: Page2Screen
     },
-    Page2: Page2Screen
-  },
-  {
-    initialRouteName: 'Page1'
-  }
+    {
+        initialRouteName: 'Page1'
+    }
 );
 
 const AppContainer = createAppContainer(TabNavigator)
 
 export default class App extends Component {
 
-  render() {
-    return (
-      <AppContainer />
-    )
-  }
+    render() {
+        return (
+                <AppContainer />
+        )
+    }
 }
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 
 expo install react-navigation
-expo install react-navigation-stack react-navigation-tabs react-native-gesture-handler react-native-reanimated
+expo install react-navigation-stack 
+expo install react-navigation-tabs 
+expo install react-native-gesture-handler 
+expo install react-native-reanimated
 npm i react-native-safe-area-context
 npm i react-native-screens
 npm i react-navigation-drawer
 npm i react-native-vector-icons
 npm i @egjs/hammerjs
+
+npm i react-navigation-material-bottom-tabs
+npm i react-native-paper
