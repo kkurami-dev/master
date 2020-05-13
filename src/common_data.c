a2727770d8394435556c92b286b95a7933ce66de
kazu <a@b.com>
2020-05-14 07:47:27 +0900

    データ作成処理部分

--- src/common_data.c
+++ src/common_data.c
@@ -1,15 +1,23 @@
 
-tp_data data =
+int senddata_size[] =
   {
-   {1, },
-};
+   /* 送信  */
+   100,
+   100,
+   100,
+   100,
+   100,
+  };
 
 int get_data( int count, tp_data *data )
 {
   struct timeval tv;
 
+  /* 送信データ作成 (1000回毎にデータサイズを変更)  */
   
+
+  /* https://www.mm2d.net/main/prog/c/time-04.html  */
   gettimeofday(&tv, NULL);
-  sprintf(tp_data.start_time, "%ld, %06lu\n", tv.tv_sec, tv.tv_usec);
+  sprintf(tp_data->start_time, "%ld, %06lu\n", tv.tv_sec, tv.tv_usec);
 }
 
