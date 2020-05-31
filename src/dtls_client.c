782eb5e1b18a0973196293bc13fd616917a6c335
kazu <a@b.com>
2020-05-31 11:55:54 +0900

    DTLSの昔の早いソース

--- src/dtls_client.c
+++ src/dtls_client.c
@@ -16,41 +16,40 @@
 int main(void)
 {
   int mysocket;
-  struct addrinfo server;
+  struct sockaddr_in server;
+  //struct sockaddr_in local;
 
   SSL *ssl;
   SSL_CTX *ctx;
-  SSL_SESSION *ssl_session = NULL;
-  const long flags=(SSL_OP_NO_SSLv3 | SSL_OP_NO_TLSv1 | SSL_OP_NO_TLSv1_1);
-  BIO *bio;
-
   char msg[BUFSIZE];
   char log[128];
-  int i = 0;
-  int size;
+
+  memset(&server, 0, sizeof(server));
+  server.sin_family = AF_INET;
+  if (inet_aton(HOST_IP, &server.sin_addr) == 0) {
+    fprintf(stderr, "Invalid IP Address.\n");
+    exit(EXIT_FAILURE);
+  }
+  server.sin_port = htons( TLS_PORT );
+  //sockaddr_in server = SOCKADDR_IN_INIT( AF_INET, htons(port), InAddr(HOST_IP) );
 
   /* 前準備 */
-  SSL_load_error_strings();
-  SSL_library_init();
-  ctx = SSL_CTX_new(DTLS_client_method());
+  LOG(SSL_load_error_strings());
+  LOG(SSL_library_init());
+  LOG(ctx = SSL_CTX_new(DTLS_client_method()));
 
-#if (TEST_SSL_SESSION == 1)
-  /* セッションの再開機能 */
-  SSL_CTX_set_session_cache_mode(ctx, SSL_SESS_CACHE_BOTH );
-  //SSL_CTX_sess_set_new_cb(ctx, sess_cache_new);
-  //SSL_CTX_sess_set_get_cb(ctx, sess_cache_get);
-  //SSL_CTX_sess_set_remove_cb(ctx, sess_cache_remove);
-#endif // (TEST_SSL_SESSION == 1)
-  
   /* 認証設定 */
-  LOG(SSL_CTX_set_options(ctx, flags));/* SSLv2はセキュリティ的にNGなので除く*/
+  /* クライアント認証設定 (テストなのでエラー確認のを除く) */
+  SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);/* SSLv2はセキュリティ的にNGなので除く*/
   SSL_RET(SSL_CTX_use_certificate_chain_file(ctx, C_CERT));// 証明書の登録
   SSL_RET(SSL_CTX_use_PrivateKey_file(ctx, C_KEY, SSL_FILETYPE_PEM));// 秘密鍵の登録
-  //SSL_RET(SSL_CTX_load_verify_locations(ctx, CA_PEM, NULL));// CA証明書の登録
-  //LOG(SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, verify_callback));// 証明書検証機能の有効化
-  //LOG(SSL_CTX_set_verify_depth (ctx, 2));// 証明書チェーンの深さ
-  //LOG(SSL_CTX_set_read_ahead(ctx, 1));
+  SSL_RET(SSL_CTX_load_verify_locations(ctx, CA_PEM, NULL));// CA証明書の登録
+  SSL_CTX_set_verify_depth (ctx, 2);// 証明書チェーンの深さ
+  SSL_CTX_set_read_ahead(ctx, 1);
 
+  int i = 0;
+  BIO *bio;
+  int size;
   while(1){
     size = get_data(i++, "dtls", msg, log );
     if ( 0 == size ){
@@ -58,21 +57,15 @@ int main(void)
     }
 
     /* 接続 */
-    mysocket = get_settings_fd( HOST, SOCK_DGRAM, TEST_SENDER, &server);
+    LOG(mysocket = socket(AF_INET, SOCK_DGRAM, 0));
+    //LOG(bind(mysocket, (struct sockaddr *)&server, sizeof(server))); // ?
+    LOG(connect(mysocket, (struct sockaddr*)&server, sizeof(server)));
     LOG(bio = BIO_new_dgram(mysocket, BIO_NOCLOSE));
     LOG(BIO_ctrl(bio, BIO_CTRL_DGRAM_SET_CONNECTED, 0, &server));
     LOG(ssl = SSL_new(ctx));
-    if(ssl_session) {
-      LOG(SSL_set_session(ssl, ssl_session));
-    }
+    //LOG(SSL_set_fd(ssl, mysocket));
     LOG(SSL_set_bio(ssl, bio, bio));
     LOG(SSL_connect(ssl));///
-
-#if (TEST_SSL_SESSION == 1)
-    if(!ssl_session) {
-      LOG(ssl_session = SSL_get1_session(ssl));
-    }
-#endif
 
     /* 通信 */
     do {
@@ -92,11 +85,11 @@ int main(void)
     } while (1);
 
     /* 切断 */
-    ssl_check_shutdown( ssl );
-
+    LOG(SSL_shutdown(ssl));
+    
   cleanup:
-    LOG(SSL_free(ssl));
     LOG(close(mysocket));
+    LOG(SSL_free(ssl));
 
 #if (ONE_SEND == 0)
     endprint(log);
@@ -105,12 +98,7 @@ int main(void)
 #endif// (ONE_SEND == 0)
   }
 
-  if(ssl_session) {
-    LOG(SSL_SESSION_free(ssl_session));
-  }
-  SSL_CTX_free(ctx);
-  ERR_free_strings();
+  LOG(SSL_CTX_free(ctx));
   return EXIT_SUCCESS;
 }
-
 
