9f7ca18387e4a3f4c555e63d0d843a3f5296f856
kazu <a@b.com>
2020-06-02 06:54:54 +0900

    全部テスト出来るようにソース修正など

--- batch_test.sh
+++ batch_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # -*- coding: utf-8-unix -*-
 
-set -xue
+set -ue
 
-export LD_LIBRARY_PATH=.:..:./bin
+export LD_LIBRARY_PATH=.:..:../tools/lib:./tools/lib:
 
 declare -a array=(
     "ssl"
@@ -11,31 +11,70 @@ declare -a array=(
     "tcp"
     "udp"
 )
+declare -a array_size=(
+    100
+    50
+    150
+    200
+    400
+    600
+    800
+    1000
+    2000
+    4000
+    6000
+    8000
+    10000
+)
 
-TYPE="server"
-while getopts csh OPT
-do
-    case $OPT in
-        s)  TYPE="server"
-            ;;
-        c)  TYPE="client"
-            ;;
-        h)  usage_exit
-            ;;
-        \?) usage_exit
-            ;;
-    esac
-done
+#DEBUG=echo
+DEBUG=
+PROCES=
+function exec_loop(){
+    _log_file=""
+    for i in {1..13} ; do
+        _size_key=${PROCES}_${array_size[$i - 1]}
+        _log_base=../log/log_${_size_key}
+        echo "### ${PROCES}_(server/client) $i > ${_log_base}_* "
+
+        _log_file=${_log_base}_cap.pcapng
+        #${DEBUG} sudo tshark -i lo -w ${_log_file} &
+        sleep 0.5
+
+        _log_file=${_log_base}_server.csv
+        ${DEBUG} ${PROCES}_server $i > ${_log_file} &
+        sleep 0.3
+
+        _log_file=${_log_base}_client.csv
+        ${DEBUG} ${PROCES}_client $i > ${_log_file}
+        sleep 1
+
+        ls -lhF ${_log_base}_*
+        ${DEBUG} pkill tshark
+        ${DEBUG} pkill ${PROCES}_server
+        count=`ps -ef | grep ${PROCES}_server | grep -v grep | wc -l`
+        if [ $count = 0 ]; then
+            echo "server stiopd."
+        else
+            echo "server a."
+        fi
+        echo
+        
+        exit
+    done
+}
+
 
 pushd ~/OpenSSL/bin
 if [ ! -d ../log ]; then
     mkdir ../log
+else
+    rm -rf ../log
+    mkdir ../log
 fi
-
-for ((i = 0; i < ${#array[@]}; i++)) {
-        echo "array[$i] = ${array[i]}"
-        COMMAND=${array[i]}_${TYPE}
-        ./${COMMAND} > ../log/${COMMAND}.csv
-        sleep 1
-    }
+for var in ${array[@]} ; do
+    PROCES=${var}
+    pkill ${PROCES}_server
+    exec_loop
+done
 popd
