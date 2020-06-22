b44af4eb20a415df9421db7adccd4336c0544e2c
kazu <a@b.com>
2020-06-22 21:44:22 +0900

    ACMでの証明書取得とか

--- src/request-certificate.sh
+++ src/request-certificate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# https://docs.aws.amazon.com/cli/latest/reference/acm/request-certificate.html
-
 ARM=arn:aws:acm-pca:region:account:certificate-authority/12345678-1234-1234-1234-123456789012
 
+# プライベートキーの作成
+# https://docs.aws.amazon.com/cli/latest/reference/acm/request-certificate.html
 aws acm request-certificate \
     --domain-name www.example.com \
     --idempotency-token 12563 \
@@ -17,3 +17,16 @@ aws acm request-certificate \
 #     "CertificateArn": "arn:aws:acm:region:account:certificate/12345678-1234-1234-1234-123456789012"
 # }
 #
+
+# ACM 証明書フィールドを取得する
+# <https://docs.aws.amazon.com/ja_jp/acm/latest/userguide/gs-acm-describe.html>
+aws acm describe-certificate --certificate-arn ${ARM}
+
+# CLI を使用したプライベート証明書のエクスポート
+# <https://docs.aws.amazon.com/ja_jp/acm/latest/userguide/gs-acm-export-private.html>
+aws acm export-certificate \
+    --certificate-arn ${ARM} \
+    --passphrase file://path-to-passphrase-file  \
+    | jq -r '"\(.Certificate)\(.CertificateChain)\(.PrivateKey)"'
+
+
