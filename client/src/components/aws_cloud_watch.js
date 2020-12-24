f48269ed8a885b618960df6fd1588c56a9a0e7a3
kazu <a@b.com>
2020-12-24 19:17:06 +0900

    CloudWatchログの追っかけ取得

--- client/src/components/aws_cloud_watch.js
+++ client/src/components/aws_cloud_watch.js
@@ -2,11 +2,37 @@
  * 
  */
 import React, { Component } from 'react';
+import { getLambdaLog } from '../lib/lib_aws';
+
+import "../App.css";
 
 export default class CloudWatch extends  Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        logs: false,
+    }
+  }
+
+  handleLogs = () => {
+    if(this.state.logs) {
+      this.setState({ logs: false });
+    } else {
+      this.setState({ logs: true });
+      getLambdaLog("BlockChainMain", (io) => {
+        return this.state.logs;
+      });
+      console.log("getLambdaLog()");
+    }
+  }
+
   render() {
     return (
-      <div>CloudWatch Logs</div>
+      <div>
+        <h1>CloudWatch Logs</h1>
+        <p className="attention">{this.state.logs && "Lambda 関数ログ取得中"}</p>
+        <button className="box" onClick={this.handleLogs} name="send">ログ取得</button><br/>
+      </div>
     );
   }
 }
