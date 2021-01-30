d8c401d0f2af952407757183702aa40929eeb070
kazu <a@b.com>
2021-01-31 01:55:10 +0900

    トランザクションリレー用の設定を1度だけ行う要に修正 ERC20 の初期化変更対応

--- contracts/MyToken_base.sol
+++ contracts/MyToken_base.sol
@@ -10,15 +10,17 @@ contract MyToken is ERC20 {
     // This is for debug purpose
     event Log(address from, string message);
 
-    constructor (string memory symbol, string memory token, uint8 amount) public ERC20(symbol, token, amount) {
+    constructor (string memory symbol, string memory token, uint8 amount) public ERC20(symbol, token) {
         _mint( msg.sender,
                amount * (10 ** uint256(decimals()))
                );
     }
 
-    /* function setTxRelay(address _to) public returns (bool) { */
-    /*     return true; */
-    /* } */
+    function setTxRelay(address _to) public returns (bool) {
+        require(txrel == address(0), "MyToken: txrel not null.");
+        txrel = _to;
+        return true;
+    }
 
     /**
      * @dev Transfer token for a specified address
@@ -29,6 +31,8 @@ contract MyToken is ERC20 {
         emit Log( msg.sender, "msg.sender" );
         emit Log( txrel, "txrel" );
         emit Log( _to, "to" );
+
+        require(msg.sender == txrel, "MyToken: sender not txrel addition");
 
         _transfer(_from, _to, _value);
         return true;
