3277724f473013784e637744be9e264613e32196
kazu <a@b.com>
2024-04-02 07:56:06 +0900

    JEST

--- src/App.test.js
+++ src/App.test.js
@@ -23,7 +23,7 @@ const paramMap = {
       },
     },
     {
-      a: 2,
+      a: 1,
       b: '3',
       c: [4, 5],
       d: {
@@ -33,21 +33,26 @@ const paramMap = {
   ],
 };
 function mockFunc(key, param, callback) {
+  const arr_main = paramMap[key];
+  let mode_arr = null;
+
   const is_type = (a) => {
     if (Array.isArray(a)) return 'arr';
     if ('object' === typeof a) return 'map';
     return 'prim';
   };
+  const re_set = () => {
+    const obj = arr_main.shift();
+    mode_arr = [{ f: Object.keys(obj), in: obj }];
+  };
 
-  const arr_main = paramMap[key];
-  const mode_app = [{ f: Object.keys(arr_main[0]), in: arr_main[0] }];
-
-  while (arr_main.length > 0) {
-    const { f, in:a, b = param } = mode_app[mode_app.length - 1];
+  re_set();
+  while (arr_main.length || mode_arr.length) {
+    const { f, in: a, b = param } = mode_arr[mode_arr.length - 1];
     const k = f.pop();
     if (undefined === k) {
-      mode_app.pop();
-      if( mode_app.length === 0 ) return a;// 結果を返す
+      mode_arr.pop();
+      if (mode_arr.length === 0) return a; // 結果を返す
       continue;
     }
 
@@ -56,28 +61,43 @@ function mockFunc(key, param, callback) {
 
     // パラメータに含まれるか
     if (!b[k] || type !== is_type(b[k])) {
-      arr_main.pop();
+      re_set();
       continue;
     }
 
     // オブジェクトでない場合は内容確認
-    const obj_b = param[k];
-    if (type === 'prim'){
+    const obj_b = b[k];
+    if (type === 'prim') {
       // 差異があるのでこの設定は使わない
-      if(obj_a !== obj_b) {
-        arr_main.pop();
+      if (obj_a !== obj_b) {
+        re_set();
       }
       // 同一なので次の確認へ
       continue;
     }
 
     // オブジェクト型なのでさらに詳細を確認
-    mode_app.push({ f: Object.keys(obj_a), a: obj_a, b: obj_b });
+    mode_arr.push({ f: Object.keys(obj_a), in: obj_a, b: obj_b });
   }
   return false;
 }
 
 describe('renders learn react link', () => {
+  it('a01', () => {
+    const ret = mockFunc(
+      'a',
+      {
+        a: 1,
+        b: '3',
+        c: [4, 5],
+        d: {
+          e: 6,
+        },
+      },
+      null
+    );
+    expect(ret).to.equal(false);
+  });
   it('a01', () => {
     const ret = mockFunc(
       'a',
