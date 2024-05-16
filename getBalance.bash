fff4075f8f9383efe7ea183f9daa3158dc15a3be
kazu <a@b.com>
2024-05-16 21:36:38 +0900

    eslint 対応

--- getBalance.bash
+++ getBalance.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # ポリゴンテストネットのMATICトークン量、gas手数料の情報を表示する
 WAIT=3
-IPKEY=*****************************
+IPKEY=**************************
 
 getBalance(){
     MSG=$1
@@ -22,13 +22,42 @@ getBalance(){
     BALANCE_MATIC4=$( expr ${BALANCE_MATIC2} - ${BALANCE_MATIC3} )
 
     if [ $BALANCE_MATIC2 -lt 1000 ]; then
-        RET="  $MSG $EOA: \e[31;5m$BALANCE_MATIC1.$BALANCE_MATIC4"
+        RET="  $MSG $EOA: \e[31;5m$BALANCE_MATIC1.$BALANCE_MATIC4   "
     elif [ $BALANCE_MATIC2 -lt 10000 ]; then
-        RET="  $MSG $EOA: \e[32m$BALANCE_MATIC1.$BALANCE_MATIC4"
+        RET="  $MSG $EOA: \e[32m$BALANCE_MATIC1.$BALANCE_MATIC4     "
     else
-        RET="  $MSG $EOA: $BALANCE_MATIC1.$BALANCE_MATIC4"
+        RET="  $MSG $EOA: $BALANCE_MATIC1.$BALANCE_MATIC4           "
     fi
     eval "$3=\${RET}"
+}
+
+getFee(){
+    MSG=$1
+    URL=$2
+    GAS=$(curl ${URL} -sS --insecure)
+    maxPFee=$(echo $GAS | jq -r '.safeLow.maxPriorityFee')
+    maxFee=$(echo $GAS | jq -r '.safeLow.maxFee')
+    BaseFee=$(echo $GAS | jq -r '.estimatedBaseFee')
+    blockNumber=$(echo  $GAS | jq -r '.blockNumber')
+    pf=${maxPFee%.*}
+    mf=${maxFee%.*}
+    
+    echo -ne ${MSG}
+    if [ $pf -gt 100 ]; then
+        echo -e "\n  maxPriorityFee: \e[31;5m${maxPFee}      \e[37m"
+    elif [ $pf -gt 31 ]; then
+        echo -e "\n  maxPriorityFee: \e[32m${maxPFee}        \e[37m"
+    else
+        echo -e "\n  maxPriorityFee: ${maxPFee}               "
+    fi
+    if [ $mf -gt 100 ]; then
+        echo -e "          maxFee: \e[31m${maxFee}        \e[37m"
+    elif [ $mf -gt 60 ]; then
+        echo -e "          maxFee: \e[32m${maxFee}        \e[37m"
+    else
+        echo -e "          maxFee: ${maxFee}                "
+    fi
+    echo -e "         BaseFee: ${BaseFee}     \n     blockNumber: ${blockNumber}\n"
 }
 
 watch () {
@@ -42,23 +71,10 @@ watch () {
     getBalance "OLD STG" "0x6a55b7528152b5b7c5bacfe426e3664bd91b553a" "C"
 
     OUTPUT="${A}\n${B}\n${C}"
-    echo -e "Every ${WAIT}.0s: MATIC Balance\n${OUTPUT}"
+    echo -e "Every ${WAIT}.0s: MATIC Balance\n${OUTPUT}\n"
 
-    GAS=$(curl https://gasstation-testnet.polygon.technology/amoy -sS --insecure)
-    maxPFee=$(echo $GAS | jq -r '.safeLow.maxPriorityFee')
-    maxFee=$(echo $GAS | jq -r '.safeLow.maxFee')
-    BaseFee=$(echo $GAS | jq -r '.estimatedBaseFee')
-    blockNumber=$(echo $GAS | jq -r '.blockNumber')
-
-    if [ $maxPFee -gt 100 ]; then
-        echo -e "\nmaxPriorityFee: \e[31;5m${maxPFee}\e[37m"
-    elif [ $maxPFee -gt 50 ]; then
-        echo -e "\nmaxPriorityFee: \e[32m${maxPFee}\n[37m"
-    else
-        echo -e "\nmaxPriorityFee: ${maxPFee}"
-    fi
-    echo -e "        maxFee: ${maxFee}\n       BaseFee: ${BaseFee}\n   blockNumber: ${blockNumber}"
-
+    getFee "AMOY" "https://gasstation-testnet.polygon.technology/amoy"
+    getFee "MAINNET" "https://gasstation.polygon.technology/v2"
     sleep $WAIT
   done
 }
