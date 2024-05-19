9a7c49b617e6294cedcd6085ac785e5038793e6f
kazu <a@b.com>
2024-05-20 07:42:06 +0900

    getBalance bash

--- getBalance.bash
+++ getBalance.bash
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
 # ポリゴンテストネットのMATICトークン量、gas手数料の情報を表示する
 WAIT=3
-IPKEY=**********************
+IPKEY=********************
+
+PutLine(){
+    OUT1=$1
+    OUTNUM=$(tput cols)
+    for (( i=${#OUT1}; i<$OUTNUM; i++)); do
+        OUT1="${OUT1} "
+    done
+    echo -e "${OUT1}"
+}
 
 getBalance(){
     MSG=$1
@@ -20,18 +29,19 @@ getBalance(){
     BALANCE_MATIC2=$( expr $BALANCE_DEC / 1000000000000 )
     BALANCE_MATIC3=$( expr ${BALANCE_MATIC1} \* 1000000 )
     BALANCE_MATIC4=$( expr ${BALANCE_MATIC2} - ${BALANCE_MATIC3} )
-    if [ 6 -gt ${#BALANCE_MATIC4} ]; then
+    # 不足桁数の保管
+    for (( i=${#BALANCE_MATIC4}; i<6; i++)); do
         BALANCE_MATIC4="0${BALANCE_MATIC4}"
-    fi
+    done
 
     if [ $BALANCE_MATIC2 -lt 100000 ]; then
-        RET="  $MSG $EOA: \e[31;5m$BALANCE_MATIC1.$BALANCE_MATIC4   \e[37m"
+        RET="  $MSG $EOA: \e[31;5m$BALANCE_MATIC1.$BALANCE_MATIC4 \e[37m"
     elif [ $BALANCE_MATIC2 -lt 1000000 ]; then
-        RET="  $MSG $EOA: \e[32m$BALANCE_MATIC1.$BALANCE_MATIC4     \e[37m"
+        RET="  $MSG $EOA: \e[32m$BALANCE_MATIC1.$BALANCE_MATIC4 \e[37m"
     else
-        RET="  $MSG $EOA: $BALANCE_MATIC1.$BALANCE_MATIC4           \e[37m"
+        RET="  $MSG $EOA: $BALANCE_MATIC1.$BALANCE_MATIC4 \e[37m"
     fi
-    eval "$3=\${RET}"
+    PutLine "${RET}"
 }
 
 getFee(){
@@ -47,20 +57,22 @@ getFee(){
     
     echo -ne ${MSG}
     if [ $pf -gt 100 ]; then
-        echo -e "\n  maxPriorityFee: \e[31;5m${maxPFee}      \e[37m"
+        PutLine "  maxPriorityFee: \e[31;5m${maxPFee} \e[37m"
     elif [ $pf -gt 31 ]; then
-        echo -e "\n  maxPriorityFee: \e[32m${maxPFee}        \e[37m"
+        PutLine "  maxPriorityFee: \e[32m${maxPFee} \e[37m"
     else
-        echo -e "\n  maxPriorityFee: ${maxPFee}               "
+        PutLine "  maxPriorityFee: ${maxPFee} "
     fi
     if [ $mf -gt 100 ]; then
-        echo -e "          maxFee: \e[31m${maxFee}        \e[37m"
+        PutLine "          maxFee: \e[31m${maxFee} \e[37m"
     elif [ $mf -gt 60 ]; then
-        echo -e "          maxFee: \e[32m${maxFee}        \e[37m"
+        PutLine "          maxFee: \e[32m${maxFee} \e[37m"
     else
-        echo -e "          maxFee: ${maxFee}                "
+        PutLine "          maxFee: ${maxFee}"
     fi
-    echo -e "         BaseFee: ${BaseFee}     \n     blockNumber: ${blockNumber}\n"
+    PutLine "         BaseFee: ${BaseFee}"
+    PutLine "     blockNumber: ${blockNumber}"
+    PutLine "                "
 }
 
 watch () {
@@ -69,16 +81,17 @@ watch () {
   while(true); do
     echo -e "\e[0;0H"
 
+    PutLine "Every ${WAIT}.0s: MATIC Balance"
     getBalance "NEW DEV" "0xc99c44e9115610d3e1682ffa7f2222281044e835" "A"
     getBalance "NEW STG" "0x78eC2078A739727B74bba6BFe55856f8EDc186e1" "B"
     getBalance "OLD DEV" "0x4f0710fa3a66ea1e8cb1f75f035e86597e629909" "C"
     getBalance "OLD STG" "0x6a55b7528152b5b7c5bacfe426e3664bd91b553a" "D"
-
-    OUTPUT="${A}\n${B}\n${C}\n${D}"
-    echo -e "Every ${WAIT}.0s: MATIC Balance\n${OUTPUT}                   \n"
+    PutLine    "                                                     "
 
     getFee "AMOY" "https://gasstation-testnet.polygon.technology/amoy"
     getFee "MAINNET" "https://gasstation.polygon.technology/v2"
+    PutLine    "                                                     "
+
     sleep $WAIT
   done
 }
