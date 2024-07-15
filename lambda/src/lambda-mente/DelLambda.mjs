3355a6202f2c4bb9dd76b181e807f6d69c94d721
kazu <a@b.com>
2024-07-15 19:16:52 +0900

    .aws からのMFA認証の追加

--- lambda/src/lambda-mente/DelLambda.mjs
+++ lambda/src/lambda-mente/DelLambda.mjs
@@ -4,12 +4,24 @@ import path from 'path';
 // AWS SDK V3 JavaScript のリファレンス
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/
 //   左のサービス一覧から対象サービスを選択、フィルタに操作を入れて検索
-import { fromIni } from '@aws-sdk/credential-providers';
+
+// MFA認証
+import {
+  STSClient,
+  AssumeRoleCommand,
+} from "@aws-sdk/client-sts";
+import { createInterface } from "readline";
+import * as  ini from 'ini'; // https://www.npmjs.com/package/ini
+
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-credential-providers/#fromini
+// import { fromIni } from "@aws-sdk/credential-provider-ini";
+
+// 各種AWS操作
 //import { ListTablesCommand, DescribeTableCommand, DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { LambdaClient, ListFunctionsCommand, DeleteFunctionCommand } from '@aws-sdk/client-lambda';
 import {
   CloudFormationClient,
-  ListStackCommand,
+  ListStacksCommand,
   DeleteStackCommand,
 } from '@aws-sdk/client-cloudformation';
 
@@ -17,10 +29,97 @@ import {
 // 引数の1つ目は使用するプロファイル名を指定する事
 //
 // process.argv = ['node', '$0', 'default', 'delconf'];
-const credentials = fromIni(process.argv[2]);
+const ARGV = ['node', '$0', 'jvl', 'prof'];
+//const credentials = fromIni(process.argv[2]);
+let credentials = null;
 
 const afuncs = {};
 
+////////////////////////////////////////////////////////////////////////////////
+// Nodejs でMFA認証の参考サイト
+// https://dev.classmethod.jp/articles/aws-sdk-for-javascript-v3-assume-role-and-create-ec2-instance/
+
+// Read Input : 標準入力からの文字列入力
+async function readInput( questionText ){
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve, reject) => {
+    readline.question(questionText, (answerText) => {
+      if (answerText) {
+        resolve(answerText);
+      } else {
+        reject("Failed read standard input.");
+      }
+      readline.close();
+    });
+  });
+};
+
+// Get IAM Role profile
+async function getRoleProfile( profile = "jvl" ){
+  const path = `${process.env["HOME"]}/.aws`;
+  const cdl = ini.parse(fs.readFileSync(`${path}/credentials`, 'utf-8'));
+  const cfg = ini.parse(fs.readFileSync(`${path}/config`, 'utf-8'));
+
+  // プロファイルのソース
+  const srcName = cfg[ profile ].credential_source;
+  const keys = cdl[ srcName ];
+  keys.RoleArn = cfg[ profile ].role_arn;
+  return keys;
+
+  // 使えそうで使えない
+  // const inProf = fromIni({ profile })
+  // console.log("inProf", inProf);
+}
+
+// Assume Role
+async function assumeRole(roleProfile ){
+  // Input MFA Token
+  const mfaToken = await readInput(`MFA token for ${roleProfile.mfaSerial} > `);
+
+  const stsClient = new STSClient({ region: 'ap-northeast-1' });
+  const command = new AssumeRoleCommand({
+    // The Amazon Resource Name (ARN) of the role to assume.
+    RoleArn: roleProfile.RoleArn, // "ROLE_ARN",
+    SerialNumber: roleProfile.mfaSerial,
+    TokenCode: mfaToken,
+
+    // sdk 用
+    RoleSessionName: "session1",
+    DurationSeconds: 3600, // 1H
+  });
+  const response = await stsClient.send(command);
+  console.log(response);
+}
+
+// Get Credentials
+async function getCredentials(profileName){
+  const roleProfile = await getRoleProfile(profileName);
+  if (!roleProfile) throw new Error("getRoleProfile");
+
+  const cdl = await assumeRole(roleProfile);
+  if (!cdl) throw new Error("assumeRole");
+
+  credentials = new STSClient({
+    accessKeyId: cdl.AccessKeyId,
+    secretAccessKey: cdl.SecretAccessKey,
+    sessionToken: cdl.SessionToken,
+  });
+  return credentials;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// 
+// const { defaultProvider } = require('@aws-sdk/credential-provider-node');
+// (async () => {
+//     const credential = await (defaultProvider())();
+//     console.log(credential);
+// })();
+
+////////////////////////////////////////////////////////////////////////////////
 function gitFunction(dir) {
   const files = fs.readdirSync(dir);
   files.forEach((item) => {
@@ -70,7 +169,7 @@ async function listStack() {
   const DelLambdaA = JSON.parse(bufferData.toString());
 
   const client = new CloudFormationClient({ credentials });
-  const command = new ListStackCommand({ credentials });
+  const command = new ListStacksCommand({ credentials });
 
   const fMap = await client.send(command);
   // console.log("fMap", fMap);
@@ -107,9 +206,10 @@ const FUNCS = {
   exec_la: deleteFunction,
   del_st_aconf: listStack,
   exec_st: deleteStack,
+  prof: getRoleProfile,
 };
-for (let i = 3; i < process.argv.length; i++) {
-  const fn = process.argv[i];
+for (let i = 3; i < ARGV.length; i++) {
+  const fn = ARGV[i];
   switch (fn) {
     case 'nopconf':
       fs.writeFileSync('DelLambdaA.json', JSON.stringify(afuncs, ' ', 2));
