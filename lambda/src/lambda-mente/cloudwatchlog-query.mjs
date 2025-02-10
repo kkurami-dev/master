776931fe553bcc8bf2c8c52c704c7f2b93363006
kazu <a@b.com>
2025-02-11 00:33:09 +0900

    Lambdaログのクエリーを効率的にLambdaで処理

--- lambda/src/lambda-mente/cloudwatchlog-query.mjs
+++ lambda/src/lambda-mente/cloudwatchlog-query.mjs
@@ -18,10 +18,67 @@ const s3Client = new S3Client();
 
 const LOG_QUERY = process.env.LOG_QUERY || "";
 
+function getFileName(ev, ctx){
+
+
+
+  const tmp1 = '/tmp/cloudwatchlogQuery1-';
+  const tmp2 = '/tmp/cloudwatchlogQuery2-';
+  const out = 'cloudwatchlogQuery/';
+  const idx = 'cloudwatchlogQuery/index.txt';
+  const bucketName = "";
+
+  return {idx, out, tmp1, tmp2, bucketName};
+}
+
+async function uploadFile(type, param) {
+  const Key = type ? param.out : param.idx;
+  const tmp = type ? param.tmp1 : param.tmp2;
+
+  const fileStream = fs.createReadStream(tmp);
+  const uploadParams = {
+    Bucket: param.bucketName,
+    Key,
+    Body: fileStream,
+    ContentType: "application/octet-stream", // 適宜変更
+  };
+
+  const command = new PutObjectCommand(uploadParams);
+  await s3Client.send(command);
+  console.log(`File uploaded successfully: s3://${param.bucketName}/${Key}`);
+}
+
+async function downloadFile(type, param) {
+  const Key = type ? param.out : param.idx;
+  const tmp = type ? param.tmp1 : param.tmp2;
+
+  let data = {};
+  try {
+    const command = new GetObjectCommand({ Bucket: param.bucketName, Key });
+    const { Body } = await s3Client.send(command);
+
+    const fileStream = fs.createWriteStream(tmp);
+    Body.pipe(fileStream);
+
+    console.log(`File downloaded: ${tmp}`);
+  } catch (error) {
+    console.error("Error downloading file:", error);
+  }
+  return data;
+}
+
 // ロググループをすべてリストアップする
+/*
+  setTimeout(callback, 0) を指定しても、即時実行されるわけではなく、
+  次のイベントループのタイミング で実行されます。
+  これは Node.js のイベントループ の仕組みによるもので、setTimeout は
+  タスクキュー（Timer Queue） に追加され、現在の実行中のコードがすべて
+  終わった後に処理されます。
+ */
 async function listLogGroup( cb ){
   const getFuncs = async (param) => {
     const {func, nextToken, ti} = param;
+    param.ti = null;
     clearTimeout( ti );
 
     const command = new DescribeLogGroupsCommand({ nextToken });
@@ -40,6 +97,12 @@ async function listLogGroup( cb ){
     func: getFuncs,
   };
   obj.ti = setTimeout(getFuncs, 0, obj);
+}
+
+async function queryLogs(ev, ctx){
+  const {query = null} = ev;
+
+  listLogGroup( cb );
 }
 
 // スクリプトを実行
