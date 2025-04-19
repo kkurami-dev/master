fdbd2fb4e96b5fe5d3646627bada66e2a7e329ca
kazu <a@b.com>
2025-04-19 10:50:19 +0900

    統計を作る３

--- tool/clock02.js
+++ tool/clock02.js
@@ -1,6 +1,7 @@
 //文字盤作成
 function drawClockFace() {
   const clockFace = document.querySelector(".clockFace");
+  if(!clockFace) return;
 
   //目盛り作成
   let mark_hour;
@@ -63,6 +64,7 @@ function updateClock() {
 
   // 日付表示
   const dateArea = document.querySelector(".dateArea");
+  if(!dateArea) return;
   let dateText = `${year}年${("0" + month).slice(-2)}月${("0" + date).slice(-2)}日（${dayArr[day]}）`;
   dateArea.textContent = dateText;
 
@@ -100,14 +102,105 @@ function reloadClock(){
   // JavaScript でページをリフレッシュする方法 – JS でページを再読み込みする方法
   // https://www.freecodecamp.org/japanese/news/javascript-refresh-page-how-to-reload-a-page-in-js/
   //location.reload();
-  //location.reload(true);
-  location.replace();
-  //location.href = location.href;
+  location.reload(true);
+  //location.replace();
+  // location.href = location.href;
 }
 
+const weeks = ['日', '月', '火', '水', '木', '金', '土'];
+const date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+const config = {
+  show: 3,
+}
 
+function showCalendar(year, month) {
+  for ( let i = 0; i < config.show; i++) {
+    const calendarHtml = createCalendar(year, month);
+    const sec = document.createElement('section');
+    sec.innerHTML = calendarHtml;
+    document.querySelector('#calendar').appendChild(sec);
+
+    month++
+    if (month > 12) {
+      year++
+      month = 1
+    }
+  }
+}
+
+function createCalendar(year, month) {
+  const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+  const endDate = new Date(year, month,  0) // 月の最後の日を取得
+  const endDayCount = endDate.getDate() // 月の末日
+  const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
+  const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
+  const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+  let dayCount = 1 // 日にちのカウント
+  let calendarHtml = '' // HTMLを組み立てる変数
+
+  calendarHtml += '<h1>' + year  + '/' + month + '</h1>'
+  calendarHtml += '<table>'
+
+  // 曜日の行を作成
+  for (let i = 0; i < weeks.length; i++) {
+    calendarHtml += '<td>' + weeks[i] + '</td>'
+  }
+
+  for (let w = 0; w < 6; w++) {
+    calendarHtml += '<tr>'
+    for (let d = 0; d < 7; d++) {
+      if (w == 0 && d < startDay) {
+        // 1行目で1日の曜日の前
+        let num = lastMonthendDayCount - startDay + d + 1
+        calendarHtml += '<td class="is-disabled">' + num + '</td>'
+      } else if (dayCount > endDayCount) {
+        // 末尾の日数を超えた
+        let num = dayCount - endDayCount
+        calendarHtml += '<td class="is-disabled">' + num + '</td>'
+        dayCount++
+      } else {
+        calendarHtml += `<td class="calendar_td" data-date="${year}/${month}/${dayCount}">${dayCount}</td>`
+        dayCount++
+      }
+    }
+    calendarHtml += '</tr>'
+  }
+  calendarHtml += '</table>'
+  return calendarHtml
+}
+
+// function moveCalendar(e) {
+//   document.querySelector('#calendar').innerHTML = '';
+//   if (e.target.id === 'prev') {
+//     month--
+//     if (month < 1) {
+//       year--
+//       month = 12
+//     }
+//   }
+//   if (e.target.id === 'next') {
+//     month++
+//     if (month > 12) {
+//       year++
+//       month = 1
+//     }
+//   }
+//   showCalendar(year, month)
+// }
+
+// document.querySelector('#prev').addEventListener('click', moveCalendar);
+// document.querySelector('#next').addEventListener('click', moveCalendar);
+
+// document.addEventListener("click", function(e) {
+//   if(e.target.classList.contains("calendar_td")) {
+//     alert('クリックした日付は' + e.target.dataset.date + 'です')
+//   }
+// })
 
 drawClockFace();
 setInterval(updateClock, 1000);
 updateClock();
 setInterval(reloadClock, 3500);
+showCalendar(year, month);
