8bbea50233b8dab0eb95e061360a6ab073e21b06
kazu <a@b.com>
2025-05-23 07:01:04 +0900

    自動build失敗対策

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// Import logo from './logo.svg';
+import './App.css';
+
+import Board from './component/board.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
