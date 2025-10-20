a121c0d9e684445d5f52b2f4f2a575be1d9962dd
kazu <a@b.com>
2025-10-20 20:11:20 +0900

    クローム拡張機能4

--- tool/chrome拡張/content.js
+++ tool/chrome拡張/content.js
@@ -13,72 +13,85 @@ const SKIPTAG1 = ".ytp-skip-ad";
 const SKIPTAG2 = ".ytp-skip-ad-button";
 const tmpStoped = "動画が一時停止されました。続きを視聴しますか？";
 
-// 動画か音楽かで探すIDを変更
+const MuteS = {
+  get Sponsor(){
+    return this.getSponsor();
+  },
+  get Mute(){
+    return this.getMute();
+  },
+  get MuteButton(){
+    return this.getMuteButton();
+  },
+}
+
+function setMusicFunc(){
+  MuteS.getSponsor = () =>{
+    const sponsor = document.querySelector('.badge-style-type-ad-stark');
+    if(!sponsor) return false;
+    const flg = sponsor.hasAttribute("hidden");
+    return !flg;
+  };
+  MuteS.getMuteButton = () => {
+    const muteButton3 = document.getElementById("expand-volume");
+    if (muteButton3) {
+      return muteButton3.querySelector("button");
+    }
+    return null;
+  };
+  MuteS.getMute = () => {
+    const volume1 = document.getElementById("expand-volume-slider");
+    if (volume1){
+      return volume1.ariaValueNow === "0";
+    }
+    return false;
+  };
+}
+
+function setVideoFunc(){
+  let muteButton1 = null;
+  MuteS.getSponsor = () =>{
+    const sponsor = MuteS.root.querySelector('[aria-label="スポンサー"]');
+    if(!sponsor) return false;
+
+    const sponsorHtm = sponsor.innerHTML === "スポンサー";
+    const sponsorTxt = sponsor.innerText === "スポンサー";
+    return sponsorHtm && sponsorTxt;
+  };
+  MuteS.getMuteButton = () => {
+    muteButton1 = MuteS.root.querySelector(".ytp-mute-button.ytp-button");
+    if (muteButton1) return muteButton1;
+
+    const div = MuteS.root.querySelector(".ytp-mute-button");
+    if (!div) return null;
+    muteButton1 = div.querySelector("button");
+    return muteButton1;
+  };
+  MuteS.getMute = () =>{
+    if(!muteButton1) muteButton1 = MuteS.MuteButton;
+    return muteButton1?.dataset?.tooltipTitle?.startsWith("ミュート解除")
+  };
+}
+
 const returnPlayerId = () => {
   const hostName = location.hostname;
   const subDomain = hostName.split(".")[0];
   switch (subDomain) {
-    case "music":
-      return "player";
-      break;
-    case "www":
-    default:
-      return "ytd-player";
-      break;
+  case "music":
+    setMusicFunc();
+    return "player";
+    break;
+  case "www":
+  default:
+    setVideoFunc();
+    return "ytd-player";
+    break;
   }
 };
-
-const isMusicSponsor = (root) => {
-  const sponsor = document.querySelector('.badge-style-type-ad-stark');
-  if(!sponsor) return false;
-  const flg = sponsor.hasAttribute("hidden");
-  return !flg;
-}
-const isVidoSponsor = (root) => {
-  const sponsor = root.querySelector('[aria-label="スポンサー"]');
-  if(!sponsor) return false;
-
-  const sponsorHtm = sponsor.innerHTML === "スポンサー";
-  const sponsorTxt = sponsor.innerText === "スポンサー";
-  return sponsorHtm && sponsorTxt;
-}
-const isSponsor = (root) => {
-  if (returnPlayerId() === "player") {
-    return isMusicSponsor(root);
-  }
-  return isVidoSponsor(root);
-}
-
-const getMuteButton = (root) => {
-  // Youtube Music
-  const muteButton3 = document.getElementById("expand-volume");
-  if (muteButton3) {
-    const muteButton4 = muteButton3.querySelector("button");
-    return muteButton4;
-  }
-  
-  const muteButton1 = root.querySelector(".ytp-mute-button.ytp-button");
-  if (muteButton1) return muteButton1;
-
-  const div = root.querySelector(".ytp-mute-button");
-  if (!div) return null;
-  const muteButton2 = div.querySelector("button");
-  return muteButton2;
-};
-
-const isMute = (root) => {
-  const volume1 = document.getElementById("expand-volume-slider");
-  if (volume1){
-    return volume1.ariaValueNow === "0";
-  }
-
-  const volume2 = root?.dataset?.tooltipTitle?.startsWith("ミュート解除")
-  return volume2 !== null;
-}
 
 const clickMuteButton = (action, idx, root) => {
-  const muteButton = getMuteButton(root);
-  if (isMute()) {
+  const muteButton = MuteS.MuteButton;
+  if (MuteS.Mute) {
     if (action === 0) {
       muteButton?.click();
     }
@@ -92,11 +105,12 @@ const clickMuteButton = (action, idx, root) => {
 const playerId = returnPlayerId();
 const intervalFunc = () => {
   const root = document.getElementById(playerId);
+  MuteS.root = root;
   if (root === null) {
     return;
   }
 
-  const sponsor = isSponsor(root);
+  const sponsor = MuteS.Sponsor;
   if (!sponsor) {
     clickMuteButton(0, 1, root);
     return;
